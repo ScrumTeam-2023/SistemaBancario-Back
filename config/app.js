ecb7b5c21cdf0f1b75fe7986e42a37db2ee4afb3
M <malvarez-2018477@kinal.edu.gt>
2023-06-01 22:45:49 -0600

    -(Base)
    + Se Agrego Admin Por Defecto
    - No funcional por el momento
    +se Agrego usuario

--- config/app.js
+++ config/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 
+const userRoutes = require('../src/user/user.routes')
 
 //Entity routes
 
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 const mongoConfig = require('./config/mongo');
 const app = require('./config/app');
+const userController = require('./src/user/user.controller')
 
 mongoConfig.connect()
 app.initServer();
+userController.defaultAdmin();
