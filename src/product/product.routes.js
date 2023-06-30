9039e70d5e1e9e0a931316e3c9edd9d73198414f
pvillatoro-2021626 <pvillatoro-2021626@kinal.edu.gt>
2023-06-30 09:26:16 -0600

    Productos completos

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const api = express.Router()
+const {ensureAuth} = require('../Services/autheticathed')
 const productController = require('./product.controller')
 
 api.get('/test', productController.test);
@@ -10,6 +11,6 @@ api.get('/getProducts', productController.getProducts)
 api.get('/getProduct/:id', productController.getProduct)
 api.put('/updatePrduct/:id', productController.updateProduct)
 api.delete('/deleteProduct/:id', productController.deleteProduct)
-api.put('/compra/:id', productController.compra)
+api.post('/compra/:id',ensureAuth, productController.compra)
 
-module.exports = api;
+module.exports = api;    
